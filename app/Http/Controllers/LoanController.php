1dfa0a5f1e14404b69cb7ecd6af68461106523d4
Banisaputra <bani.s26shafa@gmail.com>
2025-06-05 16:16:30 +0700

    mod: loan all

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use DateTime;
 use DateInterval;
 use App\Models\Loan;
+use App\Models\LoanAgunan;
+use App\Models\LoanPayment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class LoanController extends Controller
 {
@@ -40,7 +43,7 @@ class LoanController extends Controller
             'due_date' => 'required|date',
             'loan_tenor' => 'required|integer',
             'loan_value' => 'required|integer',
-            'interest_percent' => 'required|integer',
+            'interest_percent' => 'required|decimal:2',
         ]);
 
         $loan_code = Loan::generateCode();
@@ -50,20 +53,91 @@ class LoanController extends Controller
         $date->add(new DateInterval('P' . $request->loan_tenor . 'M'));
         $dueDate = $date->format('Ymd');
 
-        Loan::create([
-            'member_id' => $request->member_id,
-            'loan_code' => $loan_code,
-            'loan_date' => date('Ymd', strtotime($request->loan_date)),
-            'loan_tenor' => $request->loan_tenor,
-            'loan_value' => $request->loan_value,
-            'interest_percent' => $request->interest_percent,
-            'due_date' => $dueDate,
-            'loan_state' => 1,
-            'created_by' => auth()->id(),
-            'updated_by' => auth()->id(),
-        ]);
+        // check total loan
+        if ($request->loan_value > 3000000) {
+            $request->validate([
+                'ln_agunan' => 'required|string',
+                'ln_docNumber' => 'required|string',
+                'ln_docDetail' => 'required|string',
+            ], [
+                'ln_agunan.required' => 'Jaminan diperlukan untuk pinjaman ini',
+                'ln_docNumber.required' => 'Nomor Jaminan diperlukan untuk pinjaman ini',
+                'ln_docDetail.required' => 'Data Jaminan diperlukan untuk pinjaman ini',
+            ]);
+ 
+        }
 
-        return redirect()->back()->with('success', 'Data Pinjaman berhasil ditambahkan.');
+        DB::beginTransaction();
+        try {
+            //insert loan
+            $loan = Loan::create([
+                'member_id' => $request->member_id,
+                'loan_type' => "UANG",
+                'loan_code' => $loan_code,
+                'loan_date' => date('Ymd', strtotime($request->loan_date)),
+                'loan_tenor' => $request->loan_tenor,
+                'loan_value' => $request->loan_value,
+                'interest_percent' => $request->interest_percent,
+                'due_date' => $dueDate,
+                'loan_state' => 1,
+                'created_by' => auth()->id(),
+                'updated_by' => auth()->id(),
+            ]);
+    
+            if ($loan && $request->loan_value > 3000000) {
+                LoanAgunan::create([
+                    'loan_id' => $loan->id,
+                    'agunan_type' => $request->ln_agunan,
+                    'doc_number' => $request->ln_docNumber,
+                    'doc_detail' => $request->ln_docDetail,
+                    'created_by' => auth()->id(),
+                    'updated_by' => auth()->id(),
+                ]);
+            }
+    
+            // insert loan payment
+            $loan_total = $loan->loan_value;
+            $ln_date = $loan->loan_date;
+            for ($i=1; $i <= $request->loan_tenor ; $i++) { 
+                $lp_val = round($loan->loan_value / $loan->loan_tenor, 0);
+                $lp_intr = round(($lp_val*$loan->interest_percent)/100, 0);
+                $ln_remain = round($loan_total - $lp_val);
+                $pay_date = new DateTime($ln_date);
+                $lp_date = $pay_date->add(new DateInterval('P1M'))->format('Ymd');
+                LoanPayment::create([
+                    'lp_code' => LoanPayment::generateCode($lp_date),
+                    'loan_id' => $loan->id,
+                    'lp_date' => $lp_date,
+                    'lp_value' => $lp_val,
+                    'loan_interest' => $lp_intr,
+                    'loan_remaining' => $ln_remain,
+                    'lp_total' => ($lp_val+$lp_intr),
+                    'tenor_month' => $i,
+                    'lp_state' => 1,
+                    'remark' => '',
+                    'proof_of_payment' => '',
+                    'lp_forfeit' => 0,
+                    'created_by' => auth()->id(),
+                    'updated_by' => auth()->id(),
+        
+                ]);
+                $loan_total -= $lp_val;
+                $ln_date = $lp_date;
+                
+            }
+            DB::commit();
+            return redirect()->back()->with('success', 'Data Pinjaman berhasil ditambahkan.');
+            
+        } catch (\Exception $e) {
+            // Rollback transaction on error
+            DB::rollBack();
+            
+            return redirect()->back()->withInput()->with('error', $e->getMessage());
+            // return redirect()->back()->withInput()->with('error', 'Data Pinjaman gagal ditambahkan.');
+        }
+
+
+
     }
 
     /**
@@ -72,7 +146,7 @@ class LoanController extends Controller
     public function show(string $id)
     {
         $data = [
-            "loan" => Loan::with('member')->findOrFail($id),
+            "loan" => Loan::with('member','payments')->findOrFail($id),
         ];
 
         return view('loans.view', $data);

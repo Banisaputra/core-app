44c8ef03a50e5ea5684403be4d3a9176b2137025
Banisaputra <bani.s26shafa@gmail.com>
2025-10-01 15:32:20 +0700

    fix: 1

---
+++ app/Repositories/PinjamanRepository.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Sale;
+use App\Services\PeriodeService;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+
+class PinjamanRepository
+{
+    public function getPenjualanPerPeriode($tahun, $cutOffDay): Collection
+    {
+        $periode = PeriodeService::generatePeriodeTahun($tahun, $cutOffDay);
+        
+        return $periode->map(function ($periode) {
+            $data = Sale::whereBetween('sa_date', [
+               $periode['start_date']->format('Ymd'),
+               $periode['end_date']->format('Ymd')
+            ])
+            ->select(
+               DB::raw('SUM(sub_total) as total_sales'),
+               DB::raw('COUNT(*) as total_transaksi')
+            )
+            ->first();
+            
+            return [
+               'periode' => $periode['nama_periode'],
+               'start_date' => $periode['start_date']->format('d-m-Y'),
+               'end_date' => $periode['end_date']->format('d-m-Y'),
+               'total_jumlah' => $data->total_sales ?? 0,
+               'total_transaksi' => $data->total_transaksi ?? 0
+            ];
+        });
+    }
+
+    public function getPenjualanDetailPerPeriode($tahun): Collection
+    {
+        return Sale::with('items')
+            ->whereYear('sa_date', $tahun)
+            ->get()
+            ->groupBy(function ($item) {
+               $periode = PeriodeService::getPeriodeByDate($item->tanggal);
+               return $periode['nama_periode'];
+            });
+    }
+}

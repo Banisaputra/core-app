bbbdd80f509b40a2ccb223b998feb9c5261ee22d
banisaputra <bani.s26shafa@gmail.com>
2026-01-21 18:06:01 +0700

    fix: minor

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -10,7 +11,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -47,28 +48,7 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-
-    public function roles()
-    {
-        return $this->belongsToMany(Role::class);
-    }
-
-    public function hasRole($roles)
-    {
-        return $this->roles()->whereIn('name', $roles)->exists();
-    }
-
-    // Cek apakah user punya permission melalui role
-    public function hasPermission($permissionName)
-    {
-        foreach ($this->roles as $role) {
-            if ($role->permissions->contains('name', $permissionName)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
+ 
     public function member()
     {
         return $this->hasOne(Member::class);

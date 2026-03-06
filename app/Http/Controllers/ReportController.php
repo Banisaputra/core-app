186e308afdf13a1810635315ac5bc87e7e16ef6d
banisaputra <bani.s26shafa@gmail.com>
2026-03-06 09:20:58 +0700

    fix: minor

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -110,12 +110,13 @@ class ReportController extends Controller
                     })
                     ->leftJoin('saving_types', 'saving_types.id', '=', 'savings.sv_type_id')
                     ->leftJoin('positions', 'positions.id', '=', 'members.position_id')
-                    ->leftJoin('devisions', 'devision.id', '=', 'members.devision_id')
+                    ->leftJoin('devisions', 'devisions.id', '=', 'members.devision_id')
                     ->groupBy(
                         'members.id',
                         'members.nip',
                         'members.name',
-                        'positions.name'
+                        'positions.name',
+                        'devisions.name'
                     )
                     ->orderBy('members.id', 'ASC') 
                     ->get();
